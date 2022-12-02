f4b0e4522eda7815eb5f29cd60825d8ce75cd286
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-12-02 19:37:29 +0530

    /minor chnages/

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ var email = $('#email').val()
 $('#email-sub').click(() => {
     var email = $('#email').val()
     console.log(email)
+    $('#email-sub').prop('disabled', 'true')
     $.ajax({
         url: "/",
         type: 'POST',  // http method
@@ -13,10 +14,14 @@ $('#email-sub').click(() => {
             // if res is 200
             if (data == "200") {
                 $('#email').val('');
-                $('#email').prop('disabled', 'true')
-                setTimeout($('#email').prop('disabled', 'false'), 2000)
+                setTimeout($('#email-sub').prop('disabled', 'false'), 2000)
+                $('#suc').fadeIn(900);
+                $('#suc').fadeOut(2000);
             } else {
                 $('#email').val('');
+                setTimeout($('#email-sub').prop('disabled', 'false'), 2000)
+                $('#msg').fadeIn(900);
+                $('#msg').fadeOut(2000);
             }
         },
         error: function (errorMessage) {
